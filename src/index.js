d8755c6e94c77ec04c6841879f7e3a2c5711ac18
Rohit <yorohit.2002@gmail.com>
2023-08-03 17:22:00 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import DetailPage from './DetailPage';
+import LandingPage from './LandingPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+    <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/details' element={<DetailPage />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
